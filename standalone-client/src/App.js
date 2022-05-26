4250c88b3cf670faa92ff0b4384f1ac7d040705a
mubarakone <52806204+mubarakone@users.noreply.github.com>
2022-05-25 19:21:14 -0700

    first iteration

---
+++ standalone-client/src/App.js
@@ -0,0 +1,11 @@
+import {Connect} from "./walletconnect.js"
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={Connect}>Connect wallet</button>
+    </div>
+  );
+}
+
+export default App;
